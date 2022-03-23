e8b45da115ceb44c44ab202a13539ca7bbdda045
mmmmuqq <1564006443@qq.com>
2022-03-23 18:01:08 +0800

    dev

--- docs/.vuepress/sidebar.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	'/web/': require('../web/sidebar'),
-	// '/baodian/interview': require('../baodian/interview/sidebar'),
-	// '/baodian/talk': require('../baodian/talk/sidebar'),
-}	
-
--- docs/web/vscode/常用的vscode快捷键.md
+++
@@ -1,5 +0,0 @@
-# 常用的 vscode 快捷键
-
-1. ait + shift + enter 会出现中括号\
-2. f2 重命名变量
-3. 命令面板 control + p 我一般用来打开文件
